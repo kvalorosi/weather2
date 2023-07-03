f03750028bb88faa69e317fb0df7f0848c1e3724
Kaytlin Valorosi <kvalorosi@yahoo.com>
2023-07-03 13:43:47 -0700

    final

--- weather.js
+++ weather.js
@@ -1,14 +1,16 @@
 const apiKey = "42ca2561254544162bbff6a696f08f79";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=standard&q=";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
     const searchBox = document.querySelector(".search input");
     const searchBtn = document.querySelector(".search button");
+    
 
     async function checkWeather(city){
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
         var data = await response.json();
 
         console.log(data)
+       
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML =  Math.round(data.main.temp) + "°f";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
